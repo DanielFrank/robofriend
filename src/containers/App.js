f277019d3664d56de4c297950c99343ee9c807ef
Daniel Frank <dfrank@luxurypresence.com>
2022-02-19 21:47:33 -0600

    Add error overlay

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -22,22 +23,23 @@ class App extends Component {
     this.setState({searchField: event.target.value});
   }
   render () {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    const {robots, searchField } = this.state
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
     });
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else {
-      return (
+    return (robots.length === 0) ?
+      <h1>Loading</h1> :
+      (
         <div className='tc'>
           <h1 className='f1' >RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots}/>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
-    }
   }
 };
 export default App;
